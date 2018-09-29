c8e12b48d3cfd4d819e5f34839ce7b62bff69517
maxhgil <maxwellgilbert99@gmail.com>
2018-09-28 22:31:28 -0400

    Commit some login changes - make sure that when they enter wrong info it correctly says so

--- app/src/main/java/edu/gatech/cs2340/donationtracker/model/Location.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/model/Location.java
@@ -1,4 +1,27 @@
 package edu.gatech.cs2340.donationtracker.model;
 
 public class Location {
+    private String _name;
+    private double _latitude;
+    private double _longitude;
+    private String _streetAddress;
+    private String _city;
+    private String _state;
+    private int _zip;
+    private LocationType _type;
+    private String _phone_number;
+    private String _website;
+
+    public Location(String name, double latitude, double longitude, String streetAddress, String city, String state, int zip, LocationType locationType, String phoneNumber, String website) {
+        _name = name;
+        _latitude = latitude;
+        _longitude = longitude;
+        _streetAddress = streetAddress;
+        _city = city;
+        _state = state;
+        _zip = zip;
+        _type = locationType;
+        _phone_number = phoneNumber;
+        _website = website;
+    }
 }
---
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/model/LocationType.java
@@ -0,0 +1,5 @@
+package edu.gatech.cs2340.donationtracker.model;
+
+public enum LocationType {
+    DROP_OFF, STORE, WAREHOUSE;
+}
